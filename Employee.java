266c29cc6bf40e770b292e95e5ed8a8145121766
rajakhan-1107 <a1khanraja3576@gmail.com>
2022-03-17 10:41:58 +0530

    Completed Assignment 3 With all Operation

---
+++ Employee.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    String ename;
+    String emaiID;
+    int age;
+    String dob;
+
+    public Employee(String ename, String emaiID, int age, String dob) {
+        this.ename = ename;
+        this.emaiID = emaiID;
+        this.age = age;
+        this.dob = dob;
+    }
+
+    public String toString() {
+        return "ename='" + ename + '\'' +
+                ", emaiID='" + emaiID + '\'' +
+                ", age=" + age +
+                ", dob='" + dob + '\'' ;
+    }
+}
